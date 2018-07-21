5009b99ea73fce2cfee9febba5290af7da668b51
Evgeny Glukhov <evglukhov@luxoft.com>
2018-07-21 22:01:31 +0300

    fix build

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("core-js");
 var http_1 = require("./http");
 var app = new http_1.HttpServer().getApp();
 exports.app = app;
